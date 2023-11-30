de1251cb4607fa56503107323006434ec210aa2f
santaETH <sdamissie@yahoo.com>
2023-11-30 00:03:06 -0800

    Hello, write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-text = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+text = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 sys.stderr.write(text)
 sys.exit(1)
