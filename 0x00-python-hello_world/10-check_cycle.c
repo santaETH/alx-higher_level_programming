04e1714bfba9001d82455dceaf5e2eddda8c09ac
santaETH <sdamissie@yahoo.com>
2023-11-29 23:55:01 -0800

    linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,21 +9,18 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	if (list == NULL || list->next == NULL)
+	if (!list)
 		return (0);
-
-	slow = list->next;
-	fast = list->next->next;
 
 	while (slow && fast && fast->next)
 	{
-		if (slow == fast)
-			return (0);
-
 		slow = slow->next;
 		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
 	}
 
 	return (0);
