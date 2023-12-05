4309e562bdda8de33f75a5b33153b49e099ff64c
santaETH <sdamissie@yahoo.com>
2023-12-04 23:54:28 -0800

    more return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 def multiple_returns(sentence):
     my_tuple = ()
     if len(sentence) == 0:
-        my_tuple = 0, "None"
+        return 0, None
     else:
-        my_tuple = len(sentence), sentence[0]
-        return my_tuple
+        return len(sentence), sentence[0]
