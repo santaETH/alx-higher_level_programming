fc3848f8f36af8a40dc7f3f710224c8e80ab3f6c
santaETH <sdamissie@yahoo.com>
2023-12-05 05:14:07 -0800

    Linked list palindrome

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,22 +1,26 @@
-#include <python.h>
-#include <stdio.h>
+#include <Python.h>
 
 /**
  * print_python_list_info - fun that prints basic info
- * about python list.
- * @p: python list
- *
- * Return: 0
+ * @p: A PyObject list.
  */
 
 void print_python_list_info(PyObject *p)
 {
-	long int size = pyList_Size(p);
-	int i;
-	pyListobject *obj = (pyListobject *)p;
-
-	printf("[*] Size of the python List = %li\n", size);
-	printf("[*] Allocated = %li\n", obj->allocated);
+	int size, alloc, i;
+	PyObject *obj;
+	
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+	
 	for (i = 0; i < size; i++)
-		printf("Element %i: %s\n", i, py_TYPE(obj->ob_item[i])->tp_name);
+	{
+		printf("Element %d: ", i);
+		
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
 }
+
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -2,6 +2,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
