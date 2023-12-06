f73ccb146861049283448d28ee862e69bf686821
santaETH <sdamissie@yahoo.com>
2023-12-06 03:49:20 -0800

    last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     lastdigit = number % -10
+    print("Last digit of {:d} is {:d} and is negative".format(number, lastdigit))
 else:
     lastdigit = number % 10
     if lastdigit > 5:
