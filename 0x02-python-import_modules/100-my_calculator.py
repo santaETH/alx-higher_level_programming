d2bd03dbb213b4721756496617f21605c611d711
santaETH <sdamissie@yahoo.com>
2023-12-06 07:45:15 -0800

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         sys.exit(1)
         ops = {"+": add, "_": sub, "*": mul, "/": div}
         if sys.argv[2] not in list(ops.keys()):
-            print("Unknown operatot. Available operator: +, -, * and /")
+            print("Unknown operator. Available operator: +, -, * and /")
             sys.exit(1)
             a = int(sys.argv[1])
             b = int(sys.argv[3])
-            print("{} {} {} = {}".formaat(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+            print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
