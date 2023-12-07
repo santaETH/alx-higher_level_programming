2cf02e6e6b17a96426d69c8481f031897072a4fe
santaETH <sdamissie@yahoo.com>
2023-12-07 05:50:34 -0800

    updated 0x01 if else loop code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(9):
+for i in range(10):
     for j in range(i + 1, 10):
-        if i * 10 + j < 89:
-            print("{:02d}".format(i * 10 + j), end=', ')
-        print("{:02d}".format(i, j))
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=', ')
