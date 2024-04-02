b47466784ef4f226759906e8828b4de8e48098b4
santaETH <sdmissie@yhoo.com>
2024-04-02 00:44:56 -0700

    python network

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# The sends a DELETE request to the URL passed as the first argument and displays the body of the response
-
-curl -s -X DELETE "${1}"
+# The sends a DELETE request to the URL passed as the first argument
+curl -s "$1" -X DELETE
