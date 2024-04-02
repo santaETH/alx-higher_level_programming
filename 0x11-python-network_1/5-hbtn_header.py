eb513d106364cb6370a65b7b911bcc00ce2b364b
santaETH <sdmissie@yhoo.com>
2024-04-02 07:05:23 -0700

    python scropt_1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,6 +6,12 @@ import sys
 
 
 if __name__ == "__main__":
+    import sys
+    import requests
+
     url = sys.argv[1]
+
     response = requests.get(url)
-    print(response.headers.get(X-Request-Id))
+
+    if 'X-Request-Id' in response.headers:
+        print(response.headers['X-Request-Id'])
