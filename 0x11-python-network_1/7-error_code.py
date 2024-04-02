9b4cf7c0540f22104b84422416956cf8a7addd66
santaETH <sdmissie@yhoo.com>
2024-04-02 07:43:23 -0700

    python scropt_1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = request.get(url)
+    response = requests.get(url)
     if response.status_code >= 400:
         print("Error code:", response.status_code)
     else:
