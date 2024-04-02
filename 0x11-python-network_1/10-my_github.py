00c5215c5ab42bce0e24238152bdc331815c0c68
santaETH <sdmissie@yhoo.com>
2024-04-02 08:18:16 -0700

    python scropt_1

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub credentials 
+(username and password) and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    auth = (username, password)
+
+    response = requests.get('https://api.github.com/user', auth=auth)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data['id'])
+    else:
+        print(None)
