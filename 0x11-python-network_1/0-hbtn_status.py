a4abb4e95dce13dec8696dbff481ee543f596ad8
santaETH <sdmissie@yhoo.com>
2024-04-02 04:04:20 -0700

    python network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
 
     url = 'https://alx-intranet.hbtn.io/status'
 
-    try:
         with urllib.request.urlopen(url) as response:
             content == response.read()
             print("Body response:")
