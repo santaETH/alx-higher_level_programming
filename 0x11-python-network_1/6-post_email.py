772703e1a3b35989b8b72e3b061ace8e649b48f3
santaETH <sdmissie@yhoo.com>
2024-04-02 07:31:56 -0700

    python scropt_1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     payload = ['email': email]
-    response = requests.post(url, data)
+    response = requests.post(url, data=payload)
+    print(response.text)
