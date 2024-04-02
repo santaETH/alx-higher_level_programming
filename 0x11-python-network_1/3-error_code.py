ad56376d06d234a2e01da190c66cbb5e02dcd33e
santaETH <sdmissie@yhoo.com>
2024-04-02 05:26:40 -0700

    python network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,13 +4,12 @@ A ython script that takes in a URL, sends a request to the
 URL and displays the body of the response (decoded in utf-8).
 """
 
-if __name__ == "__main__":
-import urllib.request
-import urllib.error
+from urllib import request, error
 import sys
 
+if __name__ == "__main__":
 try:
     with urllib.request.urlopen(url) as response:
         print(res.read().decode('utf-8'))
-    except error.HTTPError as error:
+except error.HTTPError as error:
         print('Error code:', error.code)
