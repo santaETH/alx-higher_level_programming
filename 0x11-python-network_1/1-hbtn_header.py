934456be61227c6ee8d809ca5e4bb7fd85409159
santaETH <sdmissie@yhoo.com>
2024-04-02 04:34:33 -0700

    python network_1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""APython script that takes in a URL, sends a request to the URL 
+and displays the value of the X-Request-Id"""
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers["X-Request-Id"])
