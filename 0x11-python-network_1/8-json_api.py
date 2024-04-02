b5181c039581a94193c45066df509c8f4e7ff21f
santaETH <sdmissie@yhoo.com>
2024-04-02 08:00:55 -0700

    python scropt_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,14 +7,18 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    # Send a GET request to the specified URL
-    response = requests.get(url)
-
-    # Check the status code
-    if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
+    if len(sys.argv) < 2:
+        q = ""
     else:
-        # Print the body of the response
-        print(response.text)
+        q = sys.argv[1]
+
+    try:
+        response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+        data = resonse.json()
+
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
