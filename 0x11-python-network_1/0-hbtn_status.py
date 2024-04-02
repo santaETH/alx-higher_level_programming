6066a78dcbb1f91938365748dbcc6bfd8ca68905
santaETH <sdmissie@yhoo.com>
2024-04-02 04:21:31 -0700

    python network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """A script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
 
 
 if __name__ == "__main__":
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-            content = res.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as request:
+            content = request.read()
             print("Body response:")
             print("\t- type: {}".format(type(content)))
             print("\t- content: {}".format(content))
