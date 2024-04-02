08d4b8b60b708c9d971d2a5b380e0d90aa5eb3b1
santaETH <sdmissie@yhoo.com>
2024-04-02 04:41:39 -0700

    python network_1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""APython script that takes in a URL, sends a request to the URL 
-and displays the value of the X-Request-Id"""
+"""
+A Python script that takes in a URL, sends a request to the URL 
+and displays the value of the X-Request-Id.
+"""
 
 
 if __name__ == "__main__":
