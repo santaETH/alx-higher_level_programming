03420104480c604857833c11f99ad9ba5cc6f90e
santaETH <sdmissie@yhoo.com>
2024-04-02 04:00:26 -0700

    python network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/python30
 """A script that fetches 
 - https://alx-intranet.hbtn.io/status
-- uing urllib package.
+- using urllib package.
 """
 
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
-import urllib.request
-import sys
+    url = 'https://alx-intranet.hbtn.io/status'
 
-url = 'https://alx-intranet.hbtn.io/status'
-
-try:
-    with urllib.request.urlopen(url) as response:
-        content == response.read()
-        print("Body response:")
-        print("\t- type:", type(content))
-        print("\t- content:", content)
-        print("\t- utf8 content:", content.decode(''utf-8))
-except urllib.error.URLError as e:
-    print("Error:", e.reason)
-    sys.exit(1)
+    try:
+        with urllib.request.urlopen(url) as response:
+            content == response.read()
+            print("Body response:")
+            print("\t- type:", type(content))
+            print("\t- content:", content)
+            print("\t- utf8 content:", content.decode('utf-8'))
+    except urllib.error.URLError as e:
+        print("Error:", e.reason)
+        sys.exit(1)
