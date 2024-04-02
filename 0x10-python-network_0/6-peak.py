e64d48668fa4d3de6f73bb35c87c72b119fa7915
santaETH <sdmissie@yhoo.com>
2024-04-02 01:29:29 -0700

    python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
 """ Finds a peak in a list of unsorted integers """
 
+
 def find_peak(list_of_integers):
+    """ finds a peak in list_of_integers"""
+
     if list_of_integers is None or list_of_integers == []:
         return None
     lo = 0
@@ -15,9 +17,9 @@ def find_peak(list_of_integers):
         return max(list_of_integers)
     if list_of_integers[mid] >= list_of_integers[mid -1] and\
             list_of_integers[mid] >= list_of_integers[mid +1]:
-                return list_of_integers[mid]
-            if mid > 0 and list_of_integers [mid] < list_of_integers[mid + 1]:
-                return find_peack(list_of_integers[mid:])
-            if mid > 0 and list_of_integers[mid] < list_of_integers[mid -1]:
-                return find_peack(list_of_integers[:mid])
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers [mid] < list_of_integers[mid + 1]:
+        return find_peack(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid -1]:
+        return find_peack(list_of_integers[:mid])
         
