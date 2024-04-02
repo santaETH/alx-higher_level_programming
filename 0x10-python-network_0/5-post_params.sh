2f6be86dbfb9a39c73673afa7074979a5d90fa06
santaETH <sdmissie@yhoo.com>
2024-04-02 01:02:36 -0700

    python network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # The script that takes the url and sends a post request
-curl -s -X -d "test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "test@gmail.com&subject=I will always be here for PLD" "$1"
