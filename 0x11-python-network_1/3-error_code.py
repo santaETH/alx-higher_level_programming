c3c133ae268584319ceeb108d9345484219a0d35
santaETH <sdmissie@yhoo.com>
2024-04-03 04:34:04 -0700

    python network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
             body = response.read().decode('utf-8')
             print(body)
     except urllib.error.HTTPError as e:
-        print(f"Error code": {e.code}")
+        print(f"Error code: {e.code}")
