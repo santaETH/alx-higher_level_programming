5fd54ac41139b7311ed24349150cb77435b05f78
santaETH <sdmissie@yhoo.com>
2024-04-03 04:49:04 -0700

    python network_1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""""The script that takes in a URL, sends a request to the URL 
-and displays the value of the variable X-Request-Id in the response header"""
+""""The script that takes in a URL, sends a req to the URL
+and disp the value of the variable X-Request-Id in the resp header
+"""
 import requests
 import sys
 
