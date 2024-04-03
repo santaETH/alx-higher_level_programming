c1e3edad68cb4e5081a1141874163758659ccb07
santaETH <sdmissie@yhoo.com>
2024-04-03 04:51:11 -0700

    python network_1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email address, 
-sends a POST request to the passed URL with the email 
+Python script that takes in a URL and an email address
+sends a POST request to the passed URL with the email
 as a parameter, and finally displays
 """
 import requests
